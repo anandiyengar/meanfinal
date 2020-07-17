c4bc9e785a551ecf919c84368853be7ebdcda098
anand iyengar <anandiyengar0@gmail.com>
2020-07-17 22:24:12 +0530

    FINALLY

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  posts = [];
   title = 'meanapp-final';
+
+  constructor(
+    private http: HttpClient
+  ){
+    this.http.get<any>("/api/user").subscribe((user)=>{
+      this.posts = user
+      console.log(user)
+    })
+  }
+
 }
